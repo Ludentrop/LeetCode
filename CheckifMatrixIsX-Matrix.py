3aadfc834df11fef7eb4f03480fa1e053ed046ff
Maier Ludentrop <agamemnonagross@gmail.com>
2023-01-27 22:38:48 +0300

    Solved one more problem

---
+++ CheckifMatrixIsX-Matrix.py
@@ -0,0 +1,16 @@
+# slow shitcode
+
+class Solution:
+    def checkXMatrix(self, grid: List[List[int]]) -> bool:
+
+        n = len(grid)
+        for i in range(n):
+            for j in range(n):
+                if i == j or i+j+1 == n:
+                    if not grid[i][j]:
+                        return False
+                else:
+                    if grid[i][j]:
+                        return False
+
+        return True
